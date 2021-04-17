8e0942d17012c4cbf718cbfabec44c73e4ecf732
vzast <61386297+vzast@users.noreply.github.com>
2021-04-17 21:02:03 +0400

    Updated 9:01 pm 04.17.

--- davaleba_8.cs
+++ davaleba_8.cs
@@ -1,49 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
 
-namespace swift_stajireba
+namespace ConsoleApp5
 {
+    public class numberone
+    {
+        
+       
+
+        private string Currency(string idetificator,string currency)
+        {
+            string curr1 = "";
+            for (int i = 0; i < currency.Length; i++)
+            {
+                if (currency[i] == idetificator[0] && currency[i + 1] == idetificator[1] && currency[i + 2] == idetificator[2])
+                    for (int k = i+10; k < currency.Length; k++)
+                    {
+                        if (currency[k] == '<' && currency[k + 1] == '/' && currency[k + 2] == 't' && currency[k + 3] == 'd' && currency[k + 4] == '>' )
+                            {
+                            Console.WriteLine(currency[k+8]);
+                            k += 10;
+                            for (int l = 3; l < 9; l++)
+                            {
+                                curr1 += currency[k + l];
+                            }
+                            break;
+                            }
+                    }
+            }
+            return curr1;
+        }
+        public double Compare(string a1,string a2,string price)
+        {
+            if (a1 == "GEL")
+                return 1 / Convert.ToDouble(Currency(a2, price));
+            else if(a2 == "GEL")
+            {
+                return Convert.ToDouble(Currency(a1, price));
+            }
+            else return Convert.ToDouble(Currency(a1, price)) / Convert.ToDouble(Currency(a2, price));
+        }
+    }
     class Program
     {
-        public static double ExchangeRate(String from, String to)
-        {
-            double u = 3.2315;
-            double e = 3.9775;
-            switch (from)
-            {
-                case "USD":
-                    if (to == "GEL")
-                    {
-                        return u;
-                    }
-                    else
-                        return u / e;
-                    break;
-                case "EUR":
-                    if(to == "GEL")
-                    {
-                        return e;
-                    }
-                    else
-                        return e / u;
-                    break;
-                case "GEL":
-                    if (to == "USD")
-                    {
-                        return u;
-                    }
-                    else
-                        return e;
-                    break;
-            }
-            return 1;
-        }
         static void Main(string[] args)
         {
-            string test = "USD";
-            string test_ = "EUR";
-            Console.WriteLine(ExchangeRate(test,test_));
-            
+            numberone comp = new numberone();
+            string ident1 = Console.ReadLine();
+            string ident2 = Console.ReadLine();
+            WebClient client = new WebClient();
+            string currency = client.DownloadString("http://www.nbg.ge/rss.php");
+            Console.WriteLine(comp.Compare(ident1,ident2,currency));
         }
+        
     }
 }
